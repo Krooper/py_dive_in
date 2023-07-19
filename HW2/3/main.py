b846049b253bb0d83c41044ad5308bb19eb63c9a
Krooper <98321262+Krooper@users.noreply.github.com>
2023-07-19 21:14:06 +0300

    edited 3rd task

--- HW2/3/main.py
+++ HW2/3/main.py
@@ -9,7 +9,21 @@
 import re, fractions
 
 
-def frac_segmenter(frac_lst: list[str]) -> dict[int:int]:
+# Поиск общего делителя
+def common(m,n):
+    while m%n != 0:
+        old_m = m
+        old_n = n
+
+        m = old_n
+        n = old_m%old_n
+    return n
+
+
+# Из списка дробей (список из строк) делаем словарь, в качестве значений которого
+# выступают списки числителя и знаменателя дроби, ключи - номер дроби.
+# *(так-то словарь тут особо не нужен, но посчитал, что было удобно итерироваться в случае кол-ва дробей больше, чем 2)
+def frac_segmenter(frac_lst: list[str]) -> dict[int:[int]]:
     segments_out = {}
     for i in range(len(frac_lst)):
         segments = frac_lst[i].split("/")
@@ -20,23 +34,33 @@ def frac_segmenter(frac_lst: list[str]) -> dict[int:int]:
     return segments_out
 
 
+# Проверка дроби на соответствие формату:
+# 1) числитель и знаменатель проверяются на то, что в них ненулевое целое положительно число
+# 2) сама дробь проверяется на то, что в ней есть слеш
 def frac_pattern_check(frac_to_check: str,
-                       pattern='(?:\d{1,2}(?:\.\d+)?|100(?:\.0+)?)/(?:\d{1,2}(?:\.\d+)?|100(?:\.0+)?)') -> bool:
-    if re.fullmatch(rf'{pattern}', frac_to_check) is None:
+                       pattern='(^[1-9]\d*)|(^([1-9][0-9]*){1,3})|(^\+?[1-9][0-9]*)') -> bool:
+    try:
+        frac_lst = frac_to_check.split("/")
+        for num in frac_lst:
+            if re.fullmatch(rf'{pattern}', num) is None:
+                return False
+        return True
+    except ValueError:
         return False
-    return True
 
 
+# Ввод дробей, выдает список из них
 def frac_input(frac_number=2) -> list[str]:
     frac_out_lst = []
     for i in range(frac_number):
-        frac = input(f"Введите {i + 1}ю дробь в формате Х/Х (Х не больше 100): ")
+        frac = input(f"Введите {i + 1}-ю дробь в формате 'Х/Х' (Х - ненулевое положительно число): ")
         while not frac_pattern_check(frac):
-            frac = input(f"Ошибка ввода!\nВведите {i + 1}-ю дробь в формате Х/Х (Х не больше 100): ")
+            frac = input(f"Ошибка ввода!\nВведите {i + 1}-ю дробь в формате 'Х/Х' (Х - ненулевое положительно число): ")
         frac_out_lst.append(frac)
     return frac_out_lst
 
 
+# Класс с магическими методами
 class MyFraction:
     def __init__(self, numerator, denominator):
         self.numerator = numerator
@@ -48,12 +72,14 @@ class MyFraction:
     def __add__(self, other):
         numerator = self.numerator * other.denominator + self.denominator * other.numerator
         denominator = self.denominator * other.denominator
-        return MyFraction(numerator, denominator)
+        com = common(numerator, denominator)
+        return MyFraction(numerator//com, denominator//com)
 
     def __mul__(self, other):
         numerator = self.numerator * other.numerator
         denominator = self.denominator * other.denominator
-        return MyFraction(numerator, denominator)
+        com = common(numerator, denominator)
+        return MyFraction(numerator//com, denominator//com)
 
 
 if __name__ == '__main__':
