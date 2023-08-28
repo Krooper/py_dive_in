a2eea369f1d3e4608d70bbba4aa63b7568107e16
Krooper <98321262+Krooper@users.noreply.github.com>
2023-08-28 18:27:11 +0300

    made a decorator for quad equasion and csv

--- HW9/main.py
+++ HW9/main.py
@@ -2,28 +2,52 @@ import math
 import random
 import csv
 
+"""
+Декоратор для нахождения корней с каждой тройкой чисел из csv-файла
+"""
+def quad_eq_csv_decorator(func_to_decorate):
+    def wrapper():
+        with open('nums.csv', 'r') as csv_file:
+            reader = csv.reader(csv_file, delimiter=',')
+            for row in reader:
+                num_lst = [float(row[i]) for i in range(len(row))]
+                if num_lst:
+                    sol = func_to_decorate(num_lst)
+                    if sol:
+                        print(f'a={num_lst[0]}, b={num_lst[1]}, c={num_lst[2]}\nРешение: {sol}\n')
+                    else:
+                        print('Решения нет!\n')
+    return wrapper
+
 
 """
 Нахождение корней квадратного уравнения
 """
-def quad_eq_sol(a: float, b: float, c: float) -> list:
+@quad_eq_csv_decorator
+def quad_eq_sol(nums: list[float]) -> list:
+    a = nums[0]
+    b = nums[1]
+    c = nums[2]
     discr = b ** 2 - 4 * a * c
     solution = []
-    if discr > 0:
-        solution.append((-b + math.sqrt(discr)) / (2 * a))
-        solution.append((-b - math.sqrt(discr)) / (2 * a))
-    elif discr == 0:
-        solution.append(-b / (2 * a))
+    if a != 0:
+        if discr > 0:
+            solution.append((-b + math.sqrt(discr)) / (2 * a))
+            solution.append((-b - math.sqrt(discr)) / (2 * a))
+        elif discr == 0:
+            solution.append(-b / (2 * a))
+        else:
+            return solution
     else:
-        return solution
-
+        solution.append(-c/b)
     return solution
+
 
 """
 Случайное число с плавающей запятой в диапазон от 1 до 100
 """
 def rand_num_gen() -> float:
-    return round(random.random()*random.randint(10, 1000), 2)
+    return round(random.random() * random.randint(10, 1000), 1)
 
 
 """
@@ -33,10 +57,10 @@ def csv_save(nums=3) -> None:
     with open('nums.csv', 'w+') as csv_file:
         writer = csv.writer(csv_file)
         for i in range(random.randint(100, 1000)):
-            rand_nums = (rand_num_gen() for i in range(nums))
+            rand_nums = [rand_num_gen() for i in range(nums)]
             writer.writerow(rand_nums)
 
 
 if __name__ == '__main__':
-    print(quad_eq_sol(1.5, 3.5, 2))
     csv_save()
+    quad_eq_sol()
