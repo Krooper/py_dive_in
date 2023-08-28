b759b84672b580ca784511ade6081a9ade10ef7c
Krooper <98321262+Krooper@users.noreply.github.com>
2023-08-28 19:27:27 +0300

    made json_save decorator

--- HW9/main.py
+++ HW9/main.py
@@ -1,30 +1,43 @@
 import math
 import random
 import csv
+import json
+"""
+Декоратор для записи параметров и результата в json (получается как бы "декоратор для декоратора")
+"""
+def json_save(func_to_decorate):
+    def json_wrapper():
+        data = func_to_decorate()
+        json_data = json.dumps(data, indent='\t')
+        with open('data.json', 'w') as json_file:
+            json_file.write(json_data)
+    return json_wrapper
+
 
 """
 Декоратор для нахождения корней с каждой тройкой чисел из csv-файла
 """
 def quad_eq_csv_decorator(func_to_decorate):
-    def wrapper():
+    def eq_wrapper():
+        sol_dict = {}
         with open('nums.csv', 'r') as csv_file:
             reader = csv.reader(csv_file, delimiter=',')
             for row in reader:
                 num_lst = [float(row[i]) for i in range(len(row))]
                 if num_lst:
                     sol = func_to_decorate(num_lst)
-                    if sol:
-                        print(f'a={num_lst[0]}, b={num_lst[1]}, c={num_lst[2]}\nРешение: {sol}\n')
-                    else:
-                        print('Решения нет!\n')
-    return wrapper
+                    print(f'a={sol[0]}, b={sol[1]}, c={sol[2]}\nРешение: {sol[3]}\n')
+                    sol_dict[f'{sol[0]}, {sol[1]}, {sol[2]}'] = sol[3]
+        return sol_dict
+    return eq_wrapper
 
 
 """
 Нахождение корней квадратного уравнения
 """
+@json_save
 @quad_eq_csv_decorator
-def quad_eq_sol(nums: list[float]) -> list:
+def quad_eq_sol(nums: list[float]) -> tuple:
     a = nums[0]
     b = nums[1]
     c = nums[2]
@@ -37,10 +50,10 @@ def quad_eq_sol(nums: list[float]) -> list:
         elif discr == 0:
             solution.append(-b / (2 * a))
         else:
-            return solution
+            return a, b, c, 'No solution'
     else:
         solution.append(-c/b)
-    return solution
+    return a, b, c, solution
 
 
 """
